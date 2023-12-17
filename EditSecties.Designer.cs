7d6bea0ea26bd8c093e55c51660f1f27cb7f6788
ronald majoor <r.majoor@gmail.com>
2023-12-17 23:51:46 +0100

    Edit Insatlaties en secties gereed

--- EditSecties.Designer.cs
+++ EditSecties.Designer.cs
@@ -37,6 +37,11 @@
             this.ButNewSec = new System.Windows.Forms.Button();
             this.ButDeleteSec = new System.Windows.Forms.Button();
             this.ButSaveSec = new System.Windows.Forms.Button();
+            this.ButNewInstall = new System.Windows.Forms.Button();
+            this.ButSaveInstall = new System.Windows.Forms.Button();
+            this.ButDelInstall = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSecties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewInstal)).BeginInit();
             this.SuspendLayout();
@@ -64,7 +69,7 @@
             this.dataGridViewInstal.AllowUserToResizeRows = false;
             this.dataGridViewInstal.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewInstal.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewInstal.Location = new System.Drawing.Point(326, 12);
+            this.dataGridViewInstal.Location = new System.Drawing.Point(351, 12);
             this.dataGridViewInstal.MultiSelect = false;
             this.dataGridViewInstal.Name = "dataGridViewInstal";
             this.dataGridViewInstal.ReadOnly = true;
@@ -95,7 +100,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(323, 337);
+            this.label2.Location = new System.Drawing.Point(348, 337);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(68, 18);
             this.label2.TabIndex = 4;
@@ -104,7 +109,7 @@
             // TextBoxInstall
             // 
             this.TextBoxInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextBoxInstall.Location = new System.Drawing.Point(406, 334);
+            this.TextBoxInstall.Location = new System.Drawing.Point(431, 334);
             this.TextBoxInstall.Name = "TextBoxInstall";
             this.TextBoxInstall.Size = new System.Drawing.Size(269, 24);
             this.TextBoxInstall.TabIndex = 5;
@@ -118,6 +123,7 @@
             this.ButNewSec.TabIndex = 6;
             this.ButNewSec.Text = "Nieuw";
             this.ButNewSec.UseVisualStyleBackColor = true;
+            this.ButNewSec.Click += new System.EventHandler(this.ButNewSec_Click);
             // 
             // ButDeleteSec
             // 
@@ -128,6 +134,7 @@
             this.ButDeleteSec.TabIndex = 6;
             this.ButDeleteSec.Text = "Delete";
             this.ButDeleteSec.UseVisualStyleBackColor = true;
+            this.ButDeleteSec.Click += new System.EventHandler(this.ButDeleteSec_Click);
             // 
             // ButSaveSec
             // 
@@ -138,14 +145,67 @@
             this.ButSaveSec.TabIndex = 6;
             this.ButSaveSec.Text = "Save";
             this.ButSaveSec.UseVisualStyleBackColor = true;
+            this.ButSaveSec.Click += new System.EventHandler(this.ButSaveSec_Click);
+            // 
+            // ButNewInstall
+            // 
+            this.ButNewInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButNewInstall.Location = new System.Drawing.Point(431, 375);
+            this.ButNewInstall.Name = "ButNewInstall";
+            this.ButNewInstall.Size = new System.Drawing.Size(269, 50);
+            this.ButNewInstall.TabIndex = 6;
+            this.ButNewInstall.Text = "Nieuw";
+            this.ButNewInstall.UseVisualStyleBackColor = true;
+            this.ButNewInstall.Click += new System.EventHandler(this.ButNewInstall_Click);
+            // 
+            // ButSaveInstall
+            // 
+            this.ButSaveInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButSaveInstall.Location = new System.Drawing.Point(431, 431);
+            this.ButSaveInstall.Name = "ButSaveInstall";
+            this.ButSaveInstall.Size = new System.Drawing.Size(269, 50);
+            this.ButSaveInstall.TabIndex = 6;
+            this.ButSaveInstall.Text = "Save";
+            this.ButSaveInstall.UseVisualStyleBackColor = true;
+            this.ButSaveInstall.Click += new System.EventHandler(this.ButSaveInstall_Click);
+            // 
+            // ButDelInstall
+            // 
+            this.ButDelInstall.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButDelInstall.Location = new System.Drawing.Point(431, 487);
+            this.ButDelInstall.Name = "ButDelInstall";
+            this.ButDelInstall.Size = new System.Drawing.Size(269, 50);
+            this.ButDelInstall.TabIndex = 6;
+            this.ButDelInstall.Text = "Delete";
+            this.ButDelInstall.UseVisualStyleBackColor = true;
+            this.ButDelInstall.Click += new System.EventHandler(this.ButDelInstall_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(1, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 545);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(323, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(391, 545);
+            this.panel2.TabIndex = 8;
             // 
             // EditSecties
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 562);
+            this.ClientSize = new System.Drawing.Size(724, 562);
+            this.Controls.Add(this.ButSaveInstall);
             this.Controls.Add(this.ButSaveSec);
+            this.Controls.Add(this.ButDelInstall);
             this.Controls.Add(this.ButDeleteSec);
+            this.Controls.Add(this.ButNewInstall);
             this.Controls.Add(this.ButNewSec);
             this.Controls.Add(this.TextBoxInstall);
             this.Controls.Add(this.label2);
@@ -153,6 +213,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridViewInstal);
             this.Controls.Add(this.dataGridViewSecties);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "EditSecties";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Edit Secties";
@@ -175,5 +237,10 @@
         private System.Windows.Forms.Button ButNewSec;
         private System.Windows.Forms.Button ButDeleteSec;
         private System.Windows.Forms.Button ButSaveSec;
+        private System.Windows.Forms.Button ButNewInstall;
+        private System.Windows.Forms.Button ButSaveInstall;
+        private System.Windows.Forms.Button ButDelInstall;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- EditSecties.cs
+++ EditSecties.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
 
 namespace Overbrugging
 {
     public partial class EditSecties : Form
     {
+        List<InstallatieOnderdeel> InstallatieLijstGefiteerd = new List<InstallatieOnderdeel>();
         public EditSecties()
         {
             InitializeComponent();
@@ -25,28 +27,129 @@ namespace Overbrugging
             dataGridViewInstal.RowHeadersVisible = false;
 
             string sectie = dataGridViewSecties.SelectedRows[0].Cells[1].Value.ToString();
+            TextBoxSectie.Text = sectie;
             FiterInstalOpSectie(sectie);
         }
 
         private void dataGridViewSecties_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             string sectie = dataGridViewSecties.Rows[e.RowIndex].Cells[1].Value.ToString();
+            TextBoxSectie.Text = sectie;
             FiterInstalOpSectie(sectie);
         }
 
         private void FiterInstalOpSectie(string sectie)
         {
             // filter op sectie
-            List<InstallatieOnderdeel> InstallatieLijstGefiteerd = new List<InstallatieOnderdeel>();
+            //InstallatieLijstGefiteerd = new List<InstallatieOnderdeel>();
 
             InstallatieLijstGefiteerd = MainForm.Main.InstallatieLijst.Where(x => x.Sectie == sectie).ToList();
             dataGridViewInstal.DataSource = InstallatieLijstGefiteerd;
+
+            if (dataGridViewInstal.SelectedRows.Count > 0)
+            {
+                TextBoxInstall.Text = dataGridViewInstal.SelectedRows[0].Cells[1].Value.ToString();
+            }
+            else
+            {
+                TextBoxInstall.Text = "";
+            }
         }
 
         private void dataGridViewInstal_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             TextBoxSectie.Text = dataGridViewInstal.Rows[e.RowIndex].Cells[2].Value.ToString();
             TextBoxInstall.Text = dataGridViewInstal.Rows[e.RowIndex].Cells[1].Value.ToString();
+        }
+
+        private void ButNewSec_Click(object sender, EventArgs e)
+        {
+            TextBoxSectie.Text = "";
+            FiterInstalOpSectie("");
+            MessageBox.Show("Druk op save na invullen nieuwe sectie");
+        }
+
+        private void ButSaveSec_Click(object sender, EventArgs e)
+        {
+            Secties N = new Secties();
+            N.Index = "@@@@";
+            N.Naam = TextBoxSectie.Text;
+
+            MainForm.Main.SectieLijst.Add(N);
+            MainForm.Main.SaveDataSecties_lijst();
+
+            EditSecties_Shown(this, null);
+        }
+
+        private void ButDeleteSec_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Secties Record = MainForm.Main.SectieLijst.First(a => a.Naam == TextBoxSectie.Text);
+                // onderdelen leeg ?
+                if(InstallatieLijstGefiteerd.Count > 0)
+                {
+                    MessageBox.Show("Verwijder eerst de installaties van deze sectie");
+                    return;
+                }
+                
+                // vraag
+                DialogResult dialogResult = MessageBox.Show($"Verwijder {Record.Naam}", "Vraagje", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    MainForm.Main.SectieLijst.Remove(Record);
+                    // save
+                    MainForm.Main.SaveDataSecties_lijst();
+                    // refresh
+                    EditSecties_Shown(this, null);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Naam {TextBoxSectie.Text} niet gevonden in lijst!");
+            }
+        }
+
+        private void ButNewInstall_Click(object sender, EventArgs e)
+        {
+            TextBoxInstall.Text = "";
+            MessageBox.Show("Druk op save na invullen nieuwe Instalatie");
+        }
+
+        private void ButSaveInstall_Click(object sender, EventArgs e)
+        {
+            InstallatieOnderdeel N = new InstallatieOnderdeel();
+            N.Index = "@@@@";
+            N.Instal = TextBoxInstall.Text;
+            N.Sectie = TextBoxSectie.Text;
+
+            MainForm.Main.InstallatieLijst.Add(N);
+            MainForm.Main.SaveDataInstallaties_lijst();
+
+            EditSecties_Shown(this, null);
+        }
+
+        private void ButDelInstall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                InstallatieOnderdeel Record = MainForm.Main.InstallatieLijst.First(a => a.Instal == TextBoxInstall.Text);
+
+                // vraag
+                DialogResult dialogResult = MessageBox.Show($"Verwijder {Record.Instal}", "Vraagje", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    MainForm.Main.InstallatieLijst.Remove(Record);
+                    // save
+                    MainForm.Main.SaveDataInstallaties_lijst();
+                    // refresh
+                    EditSecties_Shown(this, null);
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Naam {TextBoxInstall.Text} niet gevonden in lijst!");
+            }
         }
     }
 }
